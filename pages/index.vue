6b1d68cde63184e85ecbfa1713cfc0d8286bf10b
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2020-06-06 21:27:44 -0500

    Update with nice minecraft background

--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,23 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">Towerism MC</h1>
-      <h2 class="subtitle">play.towerismmc.com</h2>
+  <div>
+    <div class="hero main-hero is-fullheight is-primary">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">Towerism MC</h1>
+          <h2 class="subtitle">play.towerismmc.com</h2>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+export default {}
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style scoped>
+.main-hero {
+  background-image: url('../assets/images/minecraft-mountainous-bg.jpg');
+  background-size: cover;
 }
 </style>
