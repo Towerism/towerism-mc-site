e90f5c9cb1879a0183741246b67e0881455b291f
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2020-07-15 20:54:37 -0500

    Fix nav menu on ios and macos

--- static/js/main.js
+++ static/js/main.js
@@ -51,30 +51,32 @@
       .appendTo($body)
 
     // Panel.
-    $(
-      '<div id="navPanel">' +
-            '<nav>' +
-              $('#nav').navList() +
-            '</nav>' +
-          '</div>'
-    )
-      .appendTo($body)
-      .panel({
-        delay: 500,
-        hideOnClick: true,
-        hideOnSwipe: true,
-        resetScroll: true,
-        resetForms: true,
-        side: 'left',
-        target: $body,
-        visibleClass: 'navPanel-visible'
-      })
+    setTimeout(() => {
+      $(
+        '<div id="navPanel">' +
+              '<nav>' +
+                $('#nav').navList() +
+              '</nav>' +
+            '</div>'
+      )
+        .appendTo($body)
+        .panel({
+          delay: 500,
+          hideOnClick: true,
+          hideOnSwipe: true,
+          resetScroll: true,
+          resetForms: true,
+          side: 'left',
+          target: $body,
+          visibleClass: 'navPanel-visible'
+        })
 
-    // Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-    if (browser.os == 'wp' && browser.osVersion < 10) {
-      $('#navButton, #navPanel, #page-wrapper')
-        .css('transition', 'none')
-    }
+      // Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+      if (browser.os == 'wp' && browser.osVersion < 10) {
+        $('#navButton, #navPanel, #page-wrapper')
+          .css('transition', 'none')
+      }
+    });
 
     // Header.
     if (!browser.mobile	&&
