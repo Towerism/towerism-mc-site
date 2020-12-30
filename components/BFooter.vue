18face98f731f63bf12c57a24135f91136af2c4f
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2020-12-30 03:23:42 -0600

    Update footer

--- components/BFooter.vue
+++ components/BFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer id="footer">
     <ul class="copyright">
-      <li>&copy; 2020 Blueprints.gg</li>
+      <li>&copy; 2020 TowerismMC.com</li>
       <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
       <li>Icons: <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></li>
     </ul>
